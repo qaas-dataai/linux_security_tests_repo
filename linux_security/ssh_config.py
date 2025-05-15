2b4dc5ac2ed1c45eb21294ba6276df8c38f74f97
qaas-dataai <santhosh.skd@gmail.com>
2025-05-14 20:56:00 -0700

    updating ssh configs for jenkins support

--- linux_security/ssh_config.py
+++ linux_security/ssh_config.py
@@ -1,6 +1,7 @@
 # EC2_HOST = "54.234.195.6"
 # EC2_USER = "ubuntu"  # or ec2-user for Amazon Linux
 # KEY_PATH = "/Users/ssingirikonda/workspace/linux_security_tests_repo/flask-app-key-ec2.pem"
+# this is to support jenkins and local
 
 import os
 
