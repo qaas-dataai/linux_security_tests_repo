c2d958d9cb95ff6d46317158748b8e26a66f3ebf
qaas-dataai <santhosh.skd@gmail.com>
2025-05-22 09:36:41 -0700

    updating EC@ host after upgrade

--- linux_security/ssh_config.py
+++ linux_security/ssh_config.py
@@ -1,3 +1,3 @@
-EC2_HOST = "54.234.195.6"
+EC2_HOST = "52.207.233.146"
 EC2_USER = "ubuntu"  # or ec2-user for Amazon Linux
 KEY_PATH = "/Users/ssingirikonda/workspace/linux_security_tests_repo/flask-app-key-ec2.pem"
